120ef02841d6ada9e6765d6e21b1bdbfff403209
julieagreen <julie.a.green@gmail.com>
2016-04-19 21:34:48 -0700

    Add project files.

---
+++ Julie Green Lab 3/frmMain.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Julie_Green_Lab_3
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radAdd = new System.Windows.Forms.RadioButton();
+            this.radSubtract = new System.Windows.Forms.RadioButton();
+            this.radMultiply = new System.Windows.Forms.RadioButton();
+            this.radioDivide = new System.Windows.Forms.RadioButton();
+            this.chkMaxMaster = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.txtSecondNumber);
+            this.groupBox1.Controls.Add(this.txtFirstNumber);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 181);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(167, 111);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(152, 26);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.TabStop = false;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecondNumber.Location = new System.Drawing.Point(167, 65);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(153, 26);
+            this.txtSecondNumber.TabIndex = 2;
+            this.txtSecondNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstNumber.Location = new System.Drawing.Point(167, 23);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(152, 26);
+            this.txtFirstNumber.TabIndex = 1;
+            this.txtFirstNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(548, 115);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(109, 34);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(548, 68);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(108, 36);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioDivide);
+            this.groupBox2.Controls.Add(this.radMultiply);
+            this.groupBox2.Controls.Add(this.radSubtract);
+            this.groupBox2.Controls.Add(this.radAdd);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(402, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(125, 142);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operation";
+            // 
+            // radAdd
+            // 
+            this.radAdd.AutoSize = true;
+            this.radAdd.Location = new System.Drawing.Point(23, 25);
+            this.radAdd.Name = "radAdd";
+            this.radAdd.Size = new System.Drawing.Size(51, 20);
+            this.radAdd.TabIndex = 0;
+            this.radAdd.TabStop = true;
+            this.radAdd.Text = "Add";
+            this.radAdd.UseVisualStyleBackColor = true;
+            // 
+            // radSubtract
+            // 
+            this.radSubtract.AutoSize = true;
+            this.radSubtract.Location = new System.Drawing.Point(23, 51);
+            this.radSubtract.Name = "radSubtract";
+            this.radSubtract.Size = new System.Drawing.Size(75, 20);
+            this.radSubtract.TabIndex = 1;
+            this.radSubtract.TabStop = true;
+            this.radSubtract.Text = "Subtract";
+            this.radSubtract.UseVisualStyleBackColor = true;
+            // 
+            // radMultiply
+            // 
+            this.radMultiply.AutoSize = true;
+            this.radMultiply.Location = new System.Drawing.Point(23, 77);
+            this.radMultiply.Name = "radMultiply";
+            this.radMultiply.Size = new System.Drawing.Size(71, 20);
+            this.radMultiply.TabIndex = 2;
+            this.radMultiply.TabStop = true;
+            this.radMultiply.Text = "Multiply";
+            this.radMultiply.UseVisualStyleBackColor = true;
+            // 
+            // radioDivide
+            // 
+            this.radioDivide.AutoSize = true;
+            this.radioDivide.Location = new System.Drawing.Point(23, 102);
+            this.radioDivide.Name = "radioDivide";
+            this.radioDivide.Size = new System.Drawing.Size(65, 20);
+            this.radioDivide.TabIndex = 3;
+            this.radioDivide.TabStop = true;
+            this.radioDivide.Text = "Divide";
+            this.radioDivide.UseVisualStyleBackColor = true;
+            // 
+            // chkMaxMaster
+            // 
+            this.chkMaxMaster.AutoSize = true;
+            this.chkMaxMaster.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMaxMaster.Location = new System.Drawing.Point(402, 204);
+            this.chkMaxMaster.Name = "chkMaxMaster";
+            this.chkMaxMaster.Size = new System.Drawing.Size(251, 20);
+            this.chkMaxMaster.TabIndex = 12;
+            this.chkMaxMaster.Text = "Numbers must not be greater than 100";
+            this.chkMaxMaster.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 252);
+            this.Controls.Add(this.chkMaxMaster);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator 2.0";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioDivide;
+        private System.Windows.Forms.RadioButton radMultiply;
+        private System.Windows.Forms.RadioButton radSubtract;
+        private System.Windows.Forms.RadioButton radAdd;
+        private System.Windows.Forms.CheckBox chkMaxMaster;
+    }
+}
+
---
+++ Julie Green Lab 3/frmMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+    Project: Calculator
+    Programmer: Julie Green
+    Date: Apr 16, 2016
+    Purpose: Performs simple calculations and math operations with two number inputs.
+*/
+
+namespace Julie_Green_Lab_3
+{
+    public partial class frmMain : Form
+    {
+        private const string c_sCOPYRIGHT = "Copyright 2016 by Julie Green";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            txtResult.Clear();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dFirstNumber;
+                double dSecondNumber;
+                double dResult;
+
+                if(!double.TryParse(txtFirstNumber.Text, out dFirstNumber))
+                {
+                    MessageBox.Show("First Number contains invalid data.");
+                }
+
+                if(!double.TryParse(txtSecondNumber.Text, out dSecondNumber))
+                {
+                    MessageBox.Show("Second Number contains invalid data.");
+                }
+
+                dFirstNumber = double.Parse(txtFirstNumber.Text);
+                dSecondNumber = double.Parse(txtSecondNumber.Text);
+
+                if (radAdd.Checked)
+                {
+                    dResult = dFirstNumber + dSecondNumber;
+                    txtResult.Text = dResult.ToString("n1");
+                } else if (radSubtract.Checked)
+                {
+                    dResult = dFirstNumber - dSecondNumber;
+                    txtResult.Text = dResult.ToString("n1");
+                } else if (radMultiply.Checked)
+                {
+                    dResult = dFirstNumber * dSecondNumber;
+                    txtResult.Text = dResult.ToString("n1");
+                } else if (radioDivide.Checked)
+                {
+                    dResult = dFirstNumber / dSecondNumber;
+                    txtResult.Text = dResult.ToString("n1");
+                } else
+                {
+                    MessageBox.Show("You much check one of the operation buttons.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
